8a9d4efaf952db124dcf548e4bda8a1f710313a7
jliao <jliao@sierrawireless.com>
2016-03-16 13:42:47 +0800

    add content in Test-02.c

--- modem/Test-02.c
+++ modem/Test-02.c
@@ -3,4 +3,10 @@
 void main(void)
 {
   printf("the signal disappears in the dark\n");
+  printf("AlphaGo, AlphaGo, AlphaGo"\n);
+
+  while (i < LOOPCOUNT)
+  {
+      le_api(le_sms_createSMS());	
+  }
 }
